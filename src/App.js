d6a913becbb61bc4e1b8207c99e4939d58041f5e
GollaKrishna123 <gkrishna05348@gmail.com>
2023-04-19 14:36:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react'
+function App() {
+  const [input,setInput] = useState("");
+  const [isEqualTo,setIsEqualTo] = useState("");
+  const inputHandler = (e)=>{
+    setInput(e.target.value);
+  }
+  const isEqualHandler = ()=>{
+    setIsEqualTo(eval(input));
+  }
+  const clearHandler = ()=>{
+    setInput("");
+  }
+  return (
+    <div className="App">
+      <center>
+      <input type="text" onChange={inputHandler} value={input}/><br/>
+      <h3>Your Ans is : {isEqualTo}</h3>
+      <button onClick={()=>setInput(input+'1')}>1</button>
+      <button onClick={()=>setInput(input+'2')}>2</button>
+      <button onClick={()=>setInput(input+'3')}>3</button>
+      <button onClick={()=>setInput(input+'4')}>4</button><br/>
+
+      <button onClick={()=>setInput(input+'5')}>5</button>
+      <button onClick={()=>setInput(input+'6')}>6</button>
+      <button onClick={()=>setInput(input+'7')}>7</button>
+      <button onClick={()=>setInput(input+'8')}>8</button><br/>
+
+      <button onClick={()=>setInput(input+'9')}>9</button>
+      <button onClick={()=>setInput(input+'0')}>0</button>
+      <button onClick={()=>setInput(input+'+')}>+</button>
+      <button onClick={()=>setInput(input+'-')}>-</button><br/>
+
+      <button onClick={()=>setInput(input+'*')}>*</button>
+      <button onClick={()=>setInput(input+'/')}>/</button>
+      <button onClick={clearHandler}>clr</button>
+      <button onClick={isEqualHandler}>=</button><br/>
+      </center>
+    </div>
+  );
+}
+
+export default App;
